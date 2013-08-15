5475179bd89af172be81cd558e7e93fcfa31ae9b
rstiller <robert.stiller.mail@googlemail.com>
2013-08-16 00:36:52 +0200

    added Makefile

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,10 +1,4 @@
-Package { ensure => latest, }
 
-package { [ "git", "ruby", "ruby-dev", "rubygems1.8", ]:
-} ->
-package { [ "rake", "puppetlabs_spec_helper", "puppet", ]:
-    provider => gem,
-} ->
 class { "java":
     vendor => "oracle",
 }
